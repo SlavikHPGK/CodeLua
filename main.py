51d4ebdeb672e286023ec1e1200f18f55bf28f5a
daniloslavik <129578352+SlavikHPGK@users.noreply.github.com>
2023-04-01 08:21:11 +0000

    Uf

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
+import time
 
 class Person:
   age = 15
   height = 175
   isMale = True
   name = "Cold"
-  
+  money = 0 
 
   def __init__(self, name, age, height):
     self.name = name
     self.age = age
     self.height = height
 
+  def work(self, days):
+    while days != 0:
+      time.sleep(1)
+      self.money += 1
+      days -= 1
+
 me = Person('Cold', 15, 175)
 friend = Person('Coal', 16, 180)
 
-print(me.height)
+print(me.money)
+me.work(3)
+print(me.money)
+
+print(me.name)
+
 
 
 
