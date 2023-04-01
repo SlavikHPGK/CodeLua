bc1c860affda608345cf970f6c6121b5cb7083eb
daniloslavik <129578352+SlavikHPGK@users.noreply.github.com>
2023-04-01 07:29:30 +0000

    I do objects

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
+# Класифікація обєктів
+
 class Person:
   age = 15
   height = 175
   isMale = True
   name = "Cold"
   
+
+me = Person()
+
+class Friend:
+  age = 16
+  height = 180
+  isMale = True
+  name = "Coal"
+
+friend = Friend()
+
+class Cat:
+  age = 9
+  height = 30
+  isFemale = True
+  name = "Diamond"
+  socialstatus = "Social-Nacionalist"
+
+cat = Cat()
+
+# вивід
+
+print(me)
+print(friend)
+print(cat)
+print(me.age)
+print(friend.height)
+print(cat.isFemale)
+print(me.name)
+print(cat.socialstatus)
